64d23edfba1da7859680e76855cbd906d20e83e8
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-08 13:42:38 -0500

    tailwind configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { TailwindcssModule } from 'tailwindcss';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
-    BrowserModule,AppRoutingModule,FormsModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-import Dotenv from 'dotenv-webpack';
-
-export default {
-  // Otras configuraciones de webpack aquí...
-  plugins: [
-    new Dotenv(),
-  ],
-};
