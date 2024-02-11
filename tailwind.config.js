a8651857d5c6a6814887727310b1d37bf1f4f126
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-10 23:32:35 -0500

    updating theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      green: {
+        50:'',
+        100:'',
+        200:'',
+        300:'',
+        400:'',
+        500: '#155637',
+        600: '',
+        700: '#009246',
+        800: '',
+        900: '',
+        950: '',
+      },
+    },
+    extend: {
+      colors: {
+        'verdeone': "#155637",
+        // "009246": "#009246",
+        // "65b7b1": "#65b7b1",
+        // a3e061: "#a3e061",
+        // "133f2c": "#133f2c",
+        // "2b2b2b": "#2b2b2b",
+        // "9ba382": "#9ba382",
+        // "008540": "#008540",
+        // Agrega más colores según sea necesario
+        // blue: "#155637",
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(verdeone|current|white|purple|midnight|metal|tahiti|silver|bermuda)/,
+    },
+  ],
 };
