ce26e37faf5ed46e3f15d38907913c7b54b2880f
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-11 19:33:02 -0500

    fixing tailwindcss with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,41 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors: {
-      green: {
-        50:'',
-        100:'',
-        200:'',
-        300:'',
-        400:'',
-        500: '#155637',
-        600: '',
-        700: '#009246',
-        800: '',
-        900: '',
-        950: '',
-      },
-    },
     extend: {
       colors: {
-        'verdeone': "#155637",
-        // "009246": "#009246",
-        // "65b7b1": "#65b7b1",
-        // a3e061: "#a3e061",
-        // "133f2c": "#133f2c",
-        // "2b2b2b": "#2b2b2b",
-        // "9ba382": "#9ba382",
-        // "008540": "#008540",
-        // Agrega más colores según sea necesario
-        // blue: "#155637",
+        green: {
+          1: "#155637",
+          2: "#009246",
+          3: "#133f2c",
+          4: "#008540",
+        },
+        teal: {
+          1: "#65b7b1",
+        },
+        lime: {
+          1: "#a3e061",
+          2: "#9ba382",
+        },
+        stone: {
+          1: "#2b2b2b",
+        },
       },
     },
   },
   plugins: [],
-  safelist: [
-    {
-      pattern:
-        /(bg|text|border)-(verdeone|current|white|purple|midnight|metal|tahiti|silver|bermuda)/,
-    },
-  ],
 };
