32487a5d59ed14e4657e591ac643342a5e3c3141
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-13 19:33:40 -0500

    navbar mob - adjusting pop up

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  // assets = {
-  //   logoNewton: {
-  //     url: 'assets/navbar/logo-mob-header-newton-mun.png',
-  //     alt: 'Logo de Newton',
-  //   },
-  //   // icon: 'assets/icon.png',
-  // }; // Define la propiedad
+export class NavbarComponent implements OnInit{
+  isOpen: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  
+  toggleMenu() {
+    this.isOpen = !this.isOpen
+  }
+  
 }
