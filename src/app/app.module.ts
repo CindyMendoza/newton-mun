99e63d9d1c05b162626ed86866761bba23cafad5
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-15 11:11:12 -0500

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
-
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,11 +10,29 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { CommitteesComponent } from './components/committees/committees.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FacilitiesComponent } from './components/facilities/facilities.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-// import { TailwindcssModule } from 'tailwindcss';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, NavbarComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutusComponent,
+    ScheduleComponent,
+    CommitteesComponent,
+    GalleryComponent,
+    FacilitiesComponent,
+    ContactComponent,
+    
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,5 +42,6 @@ import { FooterComponent } from './footer/footer.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
