82636f8aa5384bcffe0ad5f76a85d658dd7eda2a
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-15 12:31:02 -0500

    creation of routes and  about, schedule components

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import { AboutusComponent } from '../components/aboutus/aboutus.component';
+import { ScheduleComponent } from '../components/schedule/schedule.component';
 import { LoginComponent } from '../login/login.component';
 import { AuthGuard } from '../guards/auth.guard';
 const routes: Routes = [
+  { path: 'schedule', component: ScheduleComponent, canActivate: [AuthGuard] },
+  { path: 'aboutus', component: AboutusComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
