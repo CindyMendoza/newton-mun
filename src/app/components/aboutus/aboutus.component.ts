203d84470b0b00cf75314e71b4c21a48162d69e7
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-18 15:37:21 -0500

    pending solution to the swiper buttons

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,33 +1,82 @@
-import { Component, AfterViewInit } from '@angular/core';
-// import Swiper from 'swiper';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
 })
-export class AboutusComponent implements AfterViewInit {
+export class AboutusComponent implements AfterViewInit, OnInit {
+  // swiper: Swiper;
+
+  SwiperOptionsOne = {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // navigation: {
+    //   nextEl: '.button-next',
+    //   prevEl: '.button-prev',
+    //   // clickable: true,
+    // },
+    // autoHeight: true,
+    // calculateHeight:true,
+
+    // observer: true,
+  };
+  // private swiper = new Swiper('.mySwiper');
+  // private swiper: Swiper;
+
+  
   SwiperOptions = {
     direction: 'horizontal',
     loop: false,
     slidesPerView: 8,
     spaceBetween: 10,
-   
   };
-  
- 
-  constructor() {}
+
+  constructor() {
+    // this.swiper = new Swiper('.mySwiper', {
+    //   // your Swiper options
+    // });
+  }
 
   ngOnInit() {
+    // this.swiper = new Swiper('.container-slide', {
+    //   navigation: {
+    //     nextEl: '.button-next',
+    //     prevEl: '.button-prev',
+    //   },
+    // });
   }
+  // onSlideNext() {
+  //   if (this.swiper)  {
+  //     this.swiper.slideNext(); // Llama al método slideNext() al hacer clic en el botón
+  //   }
+  // }
+
+  // onSlidePrev() {
+  //   console.log("test slide");
+  //   if (this.swiper) {
+  //     this.swiper.slidePrev();
+  //   }
+  // }
   ngAfterViewInit() {
-    
-    const swiperContainer = document.querySelector('swiper-container.mySwiper2');
-    if(swiperContainer){
-      Object.assign(swiperContainer,this.SwiperOptions)
+    const swiperContainer = document.querySelector(
+      'swiper-container.mySwiper2'
+    );
+    const swiperContainerOne = document.querySelector(
+      'swiper-container.mySwiper'
+    );
+    if (swiperContainer) {
+      Object.assign(swiperContainer, this.SwiperOptions);
+    }
+    if (swiperContainerOne) {
+      Object.assign(swiperContainerOne, this.SwiperOptionsOne);
       // Object.assign(swiperContainer,this.SwiperOptions2)
     }
+    // Elementos de navegación
+    // const nextButton = document.querySelector('.button-next');
+    // const prevButton = document.querySelector('.button-prev');
   }
 }
-
-
