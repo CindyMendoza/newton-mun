5ec03c8629e2d3f1c9d67b582643bde4df229905
Cindy Mendoza <mendozaic17@gmail.com>
2024-02-22 16:27:42 -0500

    finished with committees

--- src/app/components/committees/committees.component.ts
+++ src/app/components/committees/committees.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-committees',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./committees.component.css'],
 })
 export class CommitteesComponent {
+  @ViewChild('slickModal')
+  slickModal!: SlickCarouselModule;
+
+ 
   slideConfig1 = {
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -15,14 +20,32 @@ export class CommitteesComponent {
     arrows: true,
     centerMode: true,
     centerPadding: '10px',
+    adaptiveHeight: true,
+    focusOnSelect:true,
+    cssEase: 'linear',
     responsive: [
       {
         breakpoint: 1280,
         settings: {
           slidesToShow: 3,
+          centerPadding: '0px',
         },
       },
     ],
-    
   };
+  slickInit(e: any) {
+    console.log(e.slick.slickCurrentSlide());
+    const slickInstance = e.slick;
+    const siguienteSlide = slickInstance.slickNext();
+    console.log(e.slick.slickNext());
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
